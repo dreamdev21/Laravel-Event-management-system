c73ed7f5006f7fdeb505bcd7f30b5e25b1bb34a8
Dave <dave@attendize.com>
2016-03-04 01:34:23 +0000

    - Work on CLI installer

--- app/Console/Commands/Install.php
+++ app/Console/Commands/Install.php
@@ -5,7 +5,7 @@ use DB;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
 
-class Init extends Command
+class Install extends Command
 {
 	/**
 	 * The name and signature of the console command.
