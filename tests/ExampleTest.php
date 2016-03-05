9167653114505ce2dd30e92bdad199fc7aaee149
Dave <dave@attendize.com>
2016-03-05 00:17:39 +0000

    - Deleting UserTest in prep for writing tests

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -9,10 +9,7 @@ class ExampleTest extends TestCase {
 	 */
 	public function testBasicExample()
 	{
-		$response = $this->call('GET', '/');
 
-		$this->assertEquals(200, $response->getStatusCode());
-		$this->assertFalse(4);
 	}
 
 }
