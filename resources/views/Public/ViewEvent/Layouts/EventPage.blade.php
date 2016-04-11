75fe2feda1b919f0fce7bc9b8ed264d65cd58047
Dave <dave@attendize.com>
2016-04-11 13:01:13 +0100

    Removed jQuery backstretch

--- resources/views/Public/ViewEvent/Layouts/EventPage.blade.php
+++ resources/views/Public/ViewEvent/Layouts/EventPage.blade.php
@@ -62,6 +62,18 @@
             }
 
         </style>
+        @if ($event->bg_type == 'color' || Input::get('bg_color_preview'))
+            <style>body {background-color: {{(Input::get('bg_color_preview') ? '#'.Input::get('bg_color_preview') : $event->bg_color)}} !important; }</style>
+        @endif
+
+        @if (($event->bg_type == 'image' || $event->bg_type == 'custom_image' || Input::get('bg_img_preview')) && !Input::get('bg_color_preview'))
+            <style>
+                body {
+                    background: url({{(Input::get('bg_img_preview') ? '/'.Input::get('bg_img_preview') :  asset(config('attendize.cdn_url_static_assets').'/'.$event->bg_image_path))}}) no-repeat center center fixed;
+                    background-size: cover;
+                }
+            </style>
+        @endif
 
     </head>
     <body class="attendize">
@@ -72,23 +84,11 @@
         <a href="#intro" style="display:none;" class="totop"><i class="ico-angle-up"></i>
             <span style="font-size:11px;">TOP</span></a>
 
-        @if ($event->bg_type == 'color' || Input::get('bg_color_preview'))
-        <style>body {background-color: {{(Input::get('bg_color_preview') ? '#'.Input::get('bg_color_preview') : $event->bg_color)}} !important; }</style>
-        @endif
-
-        {!!HTML::script(config('attendize.cdn_url_static_assets').'/assets/javascript/frontend.js')!!}
-
-        @if (($event->bg_type == 'image' || $event->bg_type == 'custom_image' || Input::get('bg_img_preview')) && !Input::get('bg_color_preview'))
-        <script>
-            $(function() {
-            $.backstretch('{{(Input::get('bg_img_preview') ? '/'.Input::get('bg_img_preview') :  asset(config('attendize.cdn_url_static_assets').'/'.$event->bg_image_path))}}');
-            });
-        </script>
-        @endif
-
         @if(isset($secondsToExpire))
         <script>if($('#countdown')) {setCountdown($('#countdown'), {{$secondsToExpire}});}</script>
         @endif
+
+        {!!HTML::script(config('attendize.cdn_url_static_assets').'/assets/javascript/frontend.js')!!}
 
         @include('Shared.Partials.GlobalFooterJS')
     </body>
