6131cac81f0bc37af1df777d71bbad73a70e7bc3
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2016-05-29 12:08:49 -0500

    Added help for import attendees

--- resources/views/ManageEvent/Modals/ImportAttendee.blade.php
+++ resources/views/ManageEvent/Modals/ImportAttendee.blade.php
@@ -23,8 +23,8 @@
                         <div class="row">
                             <div class="col-md-12">
                                 <div class="form-group">
-                                {!! Form::label('attendees_list', 'Import File', array('class'=>'control-label required')) !!}
-								
+                                {!! Form::labelWithHelp('attendees_list', 'Import File', array('class'=>'control-label required'),
+                                    'File must be .csv and the first line must contains first_name,last_name,email') !!}
                                 {!!  Form::styledFile('attendees_list',1,array('id'=>'input-attendees_list'))  !!}
                                 </div>
                             </div>
