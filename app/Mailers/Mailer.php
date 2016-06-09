fb5d872005fe0235535e322637ec2680416c9931
Dave Earley <dave@attendize.com>
2016-06-09 13:20:46 +0100

    Add back delete Mailer file

---
+++ app/Mailers/Mailer.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Mailers;
+
+use Mail;
+
+class Mailer
+{
+    public function sendTo($toEmail, $fromEmail, $fromName, $subject, $view, $data = [], $attachment = false)
+    {
+        Mail::send($view, $data, function ($message) use ($toEmail, $fromEmail, $fromName, $subject, $attachment) {
+            $replyEmail = $fromEmail;
+            $fromEmail = config('attendize.outgoing_email');
+            if ($attachment) {
+                $message->attach($attachment);
+            }
+            $message
+                ->to($toEmail)
+                ->from($fromEmail, $fromName)
+                ->replyTo($replyEmail, $fromName)
+                ->subject($subject);
+        });
+    }
+}
