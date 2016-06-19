dca10200f4bb6995b8dd9d4ab10cecc626d2754a
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2016-06-19 11:52:45 -0500

    Use event image if present

--- app/Http/Controllers/MyBaseController.php
+++ app/Http/Controllers/MyBaseController.php
@@ -49,9 +49,15 @@ class MyBaseController extends Controller
     {
         $event = Event::scope()->findOrFail($event_id);
 
+        $image_path = $event->organiser->full_logo_path;
+        if ($event->images->first() != null) {
+          $image_path = $event->images()->first()->image_path;
+        }
+
         return array_merge([
             'event' => $event,
             'questions' => $event->questions()->get(),
+            'image_path' => $image_path,
         ], $additional_data);
     }
 
