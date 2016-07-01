ca87dd602dad6e3d743e6bca8cf9d737a3e5ed99
Dave Earley <dave@attendize.com>
2016-07-01 16:10:59 +0100

    Updated twitter widget so a widget ID is no longer required

--- resources/views/Shared/Partials/TwitterTimelinePanel.blade.php
+++ resources/views/Shared/Partials/TwitterTimelinePanel.blade.php
@@ -1,7 +1,4 @@
-<div class="panel">
-    <a class="twitter-timeline" href="https://twitter.com/{{ $twitter_account }}"
-       data-widget-id="{{ config('services.twitter.widget_id') }}"
-       data-screen-name="{{ $twitter_account }}">
-        Tweets by @ {{ $twitter_account }}</a>
-    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+<div class="panel mt10">
+    <a class="twitter-timeline" data-height="400" href="https://twitter.com/{{ $twitter_account }}">Tweets by {{ $twitter_account }}</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
 </div>
+
