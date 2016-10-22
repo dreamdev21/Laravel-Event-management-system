a3d1e946bc89c7dd077c7f5212c4ebe8ffa0a9f1
Dave Earley <dave@attendize.com>
2016-10-22 23:18:36 +0100

    Hide tickets on embedded ticket lists (closes #225)

--- app/Http/Controllers/EventViewEmbeddedController.php
+++ app/Http/Controllers/EventViewEmbeddedController.php
@@ -19,7 +19,7 @@ class EventViewEmbeddedController extends Controller
 
         $data = [
             'event'       => $event,
-            'tickets'     => $event->tickets()->orderBy('created_at', 'desc')->get(),
+            'tickets'     => $event->tickets()->where('is_hidden', 0)->orderBy('created_at', 'desc')->get(),
             'is_embedded' => '1',
         ];
 
