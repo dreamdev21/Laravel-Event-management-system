98dd9aa71d82c0330bb182ce238131837aaf1d06
Dave Earley <dave@attendize.com>
2016-10-23 00:08:31 +0100

    Added a 503 error view (closes #209)

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+    <title>
+        We'll be back soon
+    </title>
+    <style>
+        body {
+            background-color: #f9f9f9;
+            color: #333;
+            text-align: center;
+            text-shadow: 0 1px 0 #fff;
+            font-size: 1.8em;
+        }
+        .missing {
+            width: 250px;
+            margin: 0 auto;
+            margin-top: 50px;
+            padding: 40px;
+        }
+    </style>
+</head>
+<body>
+<div class="missing">
+    <h2>We'll be back soon</h2>
+    We're currently making improvements to our website.
+</div>
+</body>
+</html>
